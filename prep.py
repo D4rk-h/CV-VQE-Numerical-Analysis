38ed435824c5385c78ca12b29a06e60093a09017
D4rk-h <herreragp334@gmail.com>
2026-03-13 14:12:27 +0100

    feat: developed squeezing matrix Z to build covariance matrix at first with fixed squeezing factor

--- prep.py
+++ prep.py
@@ -6,6 +6,7 @@ def build_U(N):
     Build U as a random unitary matrix of size N x N, 
     sampled from the Haar measure.
     """
+    print(unitary_group.rvs(N))
     return unitary_group.rvs(N)
 
 def build_O(N):
@@ -27,3 +28,43 @@ def build_O(N):
     """
     U = build_U(N)
     return np.block([[np.real(U), -np.imag(U)], [np.imag(U), np.real(U)]])
+
+def build_Z(r, N):
+    """
+    Z is the squeezing matrix, defined as:
+
+        Z = diag(r1, r2, ..., rN,1/r1, 1/r2, ..., 1/rN)
+
+    where r_i is a squeezing parameter (0<=r_i<=1), and 
+    squeezes mode i at x by a factor of r_i and antisqueezes
+    at p by a factor of 1/r_i.
+
+    Now to define r, since if we do an uniform distribution 
+    for r values (e.g [0,1]) most of the interval are very little
+    squeezing factors (near zero squeezing in a lot of initializations),
+    we start from the definition of r in decibels (following the convention
+    of Stornati et al):
+        
+        rdB=-10log10(r2)=-20log10(r)
+    
+    solving for r, we get:
+        
+        r=10^(-rdB/20)
+
+    """
+    r = np.full(N, 0.5) # fixed constants for testing
+    #r_db = np.random.uniform(0, 10, N)
+    #r = 10 ** (-r_db / 20)
+    return np.diag([*r, *[1/ri for ri in r]])
+
+def build_cov(O, Z):
+    """
+    Initial state is Vacuum.
+    Covariance matrix of the state, is defined as:
+        V = O Z^2 O^T
+    where O is the passive transformation and Z is the squeezing matrix.
+    """
+    return O @ Z**2 @ O.T
+
+def build_displacement(N):
+    pass
