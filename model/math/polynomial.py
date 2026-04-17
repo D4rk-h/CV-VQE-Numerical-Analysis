d716da45c2ea582955b62df1b1408a9c7d38f2c7
D4rk-h <herreragp334@gmail.com>
2026-04-17 05:56:42 +0100

    build_alpha_from_xi and polynomial term method

--- model/math/polynomial.py
+++ model/math/polynomial.py
@@ -2,13 +2,11 @@ import numpy as np
 from model.math.gaussian import build_alpha_from_xi
 
 
-def build_polynomial(S_terms):
-    pass
-
 def build_S(O, xi, j):
     N = O.shape[0] // 2
     u = O[:N, j]
     v = O[N:, j]
     alpha = build_alpha_from_xi(xi)
     return u @ alpha + v @ np.conj(alpha)
+
 
